a90894908cb98276093057fce56a701dfefb29d1
Edward Barton <edward.barton@icloud.com>
2018-11-19 19:02:01 -0500

    Implemented filter capabilities

--- js/views/app-view.js
+++ js/views/app-view.js
@@ -2,7 +2,10 @@ const AppView = Backbone.View.extend({
   el: '.todoapp',
 
   events: {
-    'keypress .new-todo': 'createOnEnter'
+    'keypress .new-todo': 'createOnEnter',
+    'click .all': 'renderAll',
+    'click .active': 'renderActive',
+    'click .completed': 'renderCompleted'
   },
 
   initialize() {
@@ -11,9 +14,42 @@ const AppView = Backbone.View.extend({
 
     this.listenTo(todosCollection, 'add', this.addOne);
   },
-  render() {
+
+  clearList() {
+    this.$list.empty();
+    this.$el.find(`.selected`).removeClass(`selected`);
+  },
+
+  renderAll() {
+    this.clearList();
+    todosCollection.forEach(todo => {
+      this.addOne(todo);
+    });
+    $(`.all`).addClass(`selected`);
+    console.log(this);
     return this;
   },
+
+  renderActive() {
+    this.clearList();
+    todosCollection.where({completed: false}).forEach(todo => {
+      this.addOne(todo);
+    });
+    $(`.active`).addClass(`selected`);
+    console.log(this);
+    return this;
+  },
+
+  renderCompleted() {
+    this.clearList();
+    todosCollection.where({completed: true}).forEach(todo => {
+      this.addOne(todo);
+    });
+    $(`.completed`).addClass(`selected`);
+    console.log(this);
+    return this;
+  },
+
   createOnEnter(e) {
     if (e.keyCode === 13 && this.$input.val()) {
       todosCollection.add({
@@ -24,6 +60,7 @@ const AppView = Backbone.View.extend({
       this.$input.val('');
     }
   },
+
   addOne(todo) {
     const todoView = new TodoView({ model: todo });
     this.$list.append(todoView.render().el);
